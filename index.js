effb5d2053038bb468aa898d5ffd392fd85bdcb4
Ederish <63983547+Ederish@users.noreply.github.com>
2022-03-27 17:03:09 -0600

    Update index.js

--- index.js
+++ index.js
@@ -16,27 +16,24 @@ var config = {
     physics:{default:"arcade"}
 };
 var N;
+var g;
+var alien;
 var bullets;
 var ship;
 var speed;
-var stats;
-var cursors;
-var lastFired = 0;
+var  lastFired = 0;
 
 var game = new Phaser.Game(config);
 
 function preload (){
-    this.load.image('ship', 'http://examples.phaser.io/assets/sprites/thrust_ship2.png');
-    this.load.image('bullet', 'http://examples.phaser.io/assets/misc/bullet0.png');
-    this.load.image('bg', 'http://examples.phaser.io/assets/skies/starfield.png')
+    this.load.image('ship','http://examples.phaser.io/assets/sprites/thrust_ship2.png');
+    this.load.image('enemy','http://examples.phaser.io/assets/sprites/diamond.png');
+    this.load.image('bullet','http://examples.phaser.io/assets/misc/bullet0.png');
+    this.load.image('bg','https://phaser.io/images/bg-body4.jpg');
 }
+function create(){
 
-function create (){
-    
-    this.add.image(400, 300, 'bg').setDisplaySize(800, 600);
-
-
-
+this.add.tileSprite(400,300,0,0,'bg')
     var Bullet = new Phaser.Class({
 
         Extends: Phaser.GameObjects.Image,
@@ -57,7 +54,7 @@ function create (){
         },
         update: function (time, delta){
             this.y -= this.speed * delta;
-
+            
             if (this.y < -25)
             {
                 this.setActive(false);
@@ -75,12 +72,17 @@ function create (){
     //  Create the objects in advance, so they're ready and waiting in the pool
     bullets.createMultiple({ quantity: 20, active: false });
 
-    ship = this.physics.add.sprite(400, 580, 'ship').setDepth(1)
+    ship = this.physics.add.sprite(400, 580,'ship').setDepth(1)
     .setCollideWorldBounds(true);
-
+   
     cursors = this.input.keyboard.createCursorKeys();   
-
     speed = Phaser.Math.GetSpeed(300, 1);
+
+this.time.addEvent({ delay:1000, callback: enemy, callbackScope: this, loop: true });
+
+function enemy(){alien=this.physics.add.sprite(Phaser.Math.Between(1,800),0,'enemy').setGravity(Phaser.Math.Between(1,20),20);}
+this.physics.add.collider(bullets,alien,alienDestroy);
+function alienDestroy(){alien.destroy()}
 }
 function update (time,delta){ 
 
@@ -90,7 +92,7 @@ if (cursors.left.isDown||N<0){
 else if (cursors.right.isDown||N>0){
     ship.x += speed * delta;
 }
-if (cursors.space.isDown && time > lastFired){
+if(cursors.space.isDown&&time>lastFired||this.input.activePointer.isDown&&time>lastFired){
 
 var bullet = bullets.get();
 if (bullet){
